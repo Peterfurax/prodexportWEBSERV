0f8e2ab6a3bc2496ee3aff478a1f280407c434a3
MONTOYA Pierre <pmontoya@lesechos.fr>
2018-02-12 18:16:44 +0100

    'mardi'

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Cards from "./cards";
+import Cards from "./Cards";
 const Posts = ({ posts }) => (
   <span>{posts.map((post, i) => <Cards post={post} key={i} />)}</span>
 );
